68e08f7d5ed2797521f59facb72faae44300a0f7
Yumiko Tezuka <ytezuka5@gmail.com>
2019-02-14 02:35:27 +0000

    ログインフォーム

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   root 'top#index'
   get 'signup'  => 'users#new'
-   resources :users
-
+   
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  resources :users
  end
