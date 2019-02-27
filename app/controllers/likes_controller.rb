5c8643d4462305d2a521b020f1a742c9c5013692
Yumiko Tezuka <ytezuka5@gmail.com>
2019-02-27 15:21:17 +0000

    ボタンの変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,9 @@
 class LikeController < ApplicationController
     def create
       @like=Like.new(
-      user_id: @current_user.id,
-      trick_id: params[:trick_id]
-       )
+          user_id: @current_user.id,
+          trick_id: params[:trick_id]
+           )
       @like.save
 
       like = {'user_id' => @like.user_id, 'trick_id' => @like.trick_id}
