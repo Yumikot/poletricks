2a6d0b2676e0fdafca22f10c3b1a3a4238b6418f
Yumiko Tezuka <ytezuka5@gmail.com>
2019-09-05 18:12:31 +0900

    articleのフッター位置変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
     before_action :authenticate_user!
     before_action :admin_user, only: [:edit, :update, :new, :destroy]
+    layout 'nofooter'
      
     def new
         @article = Article.new
