ec49806426305a255829f77e512df26f8038a5f1
Yumiko Tezuka <ytezuka5@gmail.com>
2021-01-14 14:08:56 +0900

    mail設定dev変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,17 +58,20 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { host: 'poledance-jitenlp-yumiko55.c9users.io' }
-  config.web_console.whitelisted_ips = '219.100.29.97'
+
+  config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
+
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :domain => "gmail.com",
-      :user_name      => ENV['GMAIL_USERNAME'],
-      :password       => ENV['GMAIL_PASSWORD'],
-      :authentication => :plain,
-      :enable_starttls_auto => true
+  
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.gmail.com",
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['GMAIL_USERNAME'],
+    :password       => ENV['GMAIL_PASSWORD'],
+    :domain         => 'gmail.com',
+    #:enable_starttls_auto => true
   }
    config.active_storage.service = :amazon
    
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://poletricks-world.herokuapp.com'
+  host = 'https://poletricks.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.gmail.com",
